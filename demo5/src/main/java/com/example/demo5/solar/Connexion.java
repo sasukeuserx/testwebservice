df96eb217d7043d0bc9fef19d4656a80a33183ed
stevenGrz13 <razafimahefasteven130102@gmail.com>
2023-10-17 22:16:05 +0200

    farany fa leo be ah
    farany fa leo be aho

--- demo5/src/main/java/com/example/demo5/solar/Connexion.java
+++ demo5/src/main/java/com/example/demo5/solar/Connexion.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class Connexion {
     public static Connection getConn() throws Exception {
-        String url = "jdbc:postgresql://containers-us-west-204.railway.app:6324/railway";
+        String url = "jdbc:postgresql://containers-us-west-100.railway.app:7636/railway";
         String user = "postgres";
-        String pass = "jX0bzVl6nupZ8nrTBVx6";
+        String pass = "7yJS4KFH6oCkr3gOSnmK";
         Class.forName("org.postgresql.Driver");
         Connection conn = DriverManager.getConnection(url, user, pass);
         return conn;
