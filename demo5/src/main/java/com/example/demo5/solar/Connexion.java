d824c4ccbc476e757bda744365b1cb21d5ec215f
stevenGrz13 <razafimahefasteven130102@gmail.com>
2023-10-17 22:04:11 +0200

    modification
    modification

--- demo5/src/main/java/com/example/demo5/solar/Connexion.java
+++ demo5/src/main/java/com/example/demo5/solar/Connexion.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class Connexion {
     public static Connection getConn() throws Exception {
-        String url = "jdbc:postgresql://containers-us-west-204.railway.app:6544/railway";
+        String url = "jdbc:postgresql://containers-us-west-204.railway.app:6324/railway";
         String user = "postgres";
-        String pass = "rS3sqnWeQumUpItnFsfc";
+        String pass = "jX0bzVl6nupZ8nrTBVx6";
         Class.forName("org.postgresql.Driver");
         Connection conn = DriverManager.getConnection(url, user, pass);
         return conn;
